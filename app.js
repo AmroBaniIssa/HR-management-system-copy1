5be46d5294d5e29f2dd449fc5551791ec004fc56
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-04-13 02:52:02 +0000

    final solution fot assignmint 07

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 "use strict ";
-const employees = [];
 let main = document.getElementById("main");
 let oneCard = document.getElementById("oneCard");
 main.append(oneCard);
-function employee(
+const employees = [];
+function Employee(
   EmployeeId,
   fullName,
   Department,
   Level,
   image_URL
-  //   Salary
 ) {
   this.Employee_Id = EmployeeId;
   this.fullName = fullName;
@@ -23,30 +22,30 @@ function employee(
 ///====calculating the salary && the net salary===
 //===============
 
-employee.prototype.calculateSalary = function () {
+Employee.prototype.calculateSalary = function () {
+ 
   if (this.Level === "Senior") {
-    let salaryForSenior = Math.random() * (2000 - 1500) + 1500;
+    let salaryForSenior = Math.floor( Math.random() * (2000 - 1500) + 1500);
     let taxForSenior = 0.07 * salaryForSenior;
     this.Salary = salaryForSenior - taxForSenior;
-    console.log(this.Salary);
   }
 
   if (this.Level === "Mid-Senior") {
-    let salaryForMidSenior = Math.random() * (1500 - 1000) + 1000;
+    let salaryForMidSenior = Math.floor(Math.random() * (1500 - 1000) + 1000);
     let taxForMidSenior = 0.07 * salaryForMidSenior;
     this.Salary = salaryForMidSenior - taxForMidSenior;
   }
 
   if (this.Level === "Junior") {
-    let salaryForJunior = Math.random() * (1000 - 500) + 500;
+    let salaryForJunior =Math.floor( Math.random() * (1000 - 500) + 500);
     let taxForJunior = 0.07 * salaryForJunior;
-    this.Salary = salaryForJunior - taxForSenior;
+    this.Salary = salaryForJunior - taxForJunior;
   }
 };
 
 //====== render prototype function ======
 // ==================
-employee.prototype.render = function () {
+Employee.prototype.render = function () {
   const EmployeeId = document.createElement("h2");
   EmployeeId.textContent = this.Employee_Id;
   const fullName = document.createElement("h2");
@@ -65,22 +64,22 @@ employee.prototype.render = function () {
   oneCard.appendChild(Level);
   oneCard.appendChild(Salary);
   oneCard.appendChild(image_URL);
+  
 };
-let employee1 = new employee("1000", "Ghazi Samer", "Administration", "Senior");
-let employee2 = new employee("1001", "Lana Ali", "Finance", "Senior");
-let employee3 = new employee("1002", "Tamara Ayoub", "Marketing", "Senior");
-let employee4 = new employee(
+let employee1 = new Employee("1000", "Ghazi Samer", "Administration", "Senior");
+let employee2 = new Employee("1001", "Lana Ali", "Finance", "Senior");
+let employee3 = new Employee("1002", "Tamara Ayoub", "Marketing", "Senior");
+let employee4 = new Employee(
   "1003",
   "Safi Walid",
   "Administration",
   "Mid-Senior"
 );
-let employee5 = new employee("1004", "Omar Zaid", "Development", "Senior");
-let employee6 = new employee("1005", "Rana Saleh	", "Development", "Junior");
-let employee7 = new employee("1006", "Hadi Ahmad", "Finance", "	Mid-Senior");
+let employee5 = new Employee("1004", "Omar Zaid", "Development", "Senior");
+let employee6 = new Employee("1005", "Rana Saleh	", "Development", "Junior");
+let employee7 = new Employee("1006", "Hadi Ahmad", "Finance", "Mid-Senior");
 
 for (let i = 0; i < employees.length; i++) {
-  employee.prototype.calculateSalary();
-  // console.log(this.Salary)
+  employees[i].calculateSalary();
   employees[i].render();
 }
